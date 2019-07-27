8ab1d93238698a2389aabd85d2c1d8a1ce8c0407
shbabel <yourmediafriend@gmail.com>
2019-07-27 23:47:41 +0100

    Column Headers and Styling

---
+++ src/molecules/Dropdown/index.js
@@ -0,0 +1,102 @@
+import React from "react";
+import Nav from "../../content/testData";
+import { flow, filter } from "lodash/fp";
+import "./Dropdown.css";
+
+class Dropdown extends React.Component {
+	render() {
+		const { data, id } = this.props;
+
+		let colClass = "";
+
+		const colOne = flow(
+			filter(
+				e =>
+					!e.is_column_header &&
+					e.include_in_menu &&
+					!e.include_in_menu_column2 &&
+					!e.include_in_menu_column3
+			),
+			function(e) {
+				if (e.length) {
+					colClass = "columns-1";
+				}
+				return e;
+			}
+		)(data);
+
+		const colOneHeader = flow(
+			filter(
+				e =>
+					e.is_column_header &&
+					e.include_in_menu &&
+					!e.include_in_menu_column2 &&
+					!e.include_in_menu_column3
+			)
+		)(data);
+
+		const colTwo = flow(
+			filter(e => !e.is_column_header && e.include_in_menu_column2),
+			function(e) {
+				if (e.length) {
+					colClass = "columns-2";
+				}
+				return e;
+			}
+		)(data);
+
+		const colTwoHeader = flow(
+			filter(e => e.is_column_header && e.include_in_menu_column2),
+		)(data);
+
+		const colThree = flow(
+			filter(e => !e.is_column_header && e.include_in_menu_column3),
+			function(e) {
+				if (e.length) {
+					colClass = "columns-3";
+				}
+				return e;
+			}
+		)(data);
+
+		const colThreeHeader = flow(
+			filter(e => e.is_column_header && e.include_in_menu_column3),
+		)(data);
+
+
+		const colMenu = [{menuHeader: colOneHeader, menuList: colOne } , {menuHeader: colTwoHeader, menuList: colTwo } , {menuHeader: colThreeHeader, menuList: colThree } ];      
+
+		return (
+			<div className={`nav_dropdrown ${colClass}`} id={id}>
+				<div className={"nav_dropdrown_inner"}>
+					<div className={"nav_dropdrown_content"}>
+						{colMenu.map((e, i) => {
+							if (e.menuList.length) {
+								return (
+									<div key={i}>
+										{e.menuHeader[0] ? <h3 className="colHeader">{e.menuHeader[0].custom_category_name || e.menuHeader[0].name}</h3> : null}
+										<ul>
+											{e.menuList.map((e, i) => {
+												return (
+													<li key={i}>
+														<a href={`/${e.url_path}`}>
+															<span>
+																{e.custom_category_name || e.name}
+															</span>
+														</a>
+													</li>
+												);
+											})}
+										</ul>
+									</div>
+								);
+							}
+						})}
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Dropdown;
--- src/organisms/Navigation/index.js
+++ src/organisms/Navigation/index.js
@@ -3,103 +3,30 @@ import Nav from "../../content/testData";
 import { filter } from "lodash";
 import "./Navigaton.css";
 
+import Dropdown from "../../molecules/Dropdown";
 
-class EmptyComp extends React.Component {
-
-
-	filterCol(e) {
-
-
-		// let collAll = e.children_data.map((e, i) => {
-		// 	if (e.include_in_menu) {
-		// 		return e;
-		// 	}
-		// });
-
-
-
-
-
-
-
-		// if (collAll) {
-
-		// 	let collTwo = collAll.map((e, i) => {
-		// 		if (e.include_in_menu_column2) {
-		// 			return e;
-		// 		}
-		// 	});
-	
-			
-		// 	let collThree = collAll.map((e, i) => {
-		// 		if (e.include_in_menu_column3) {
-		// 			return e;
-		// 		}
-		// 	});
-
-		// 	console.log('collTwo', collTwo);
-		// 	console.log('collThree', collThree);
-
-		// }
-
-
-
-
-	}
-
+class Navigation extends React.Component {
 	render() {
 		return (
 			<nav>
 				<ul className="nav">
 					{Nav.navCatagories.map((e, i) => {
-
-						// Dropdown Structure
-						const colAll = filter(e.children_data, e => e.include_in_menu);
-						const colTwo = filter(e.children_data, e => e.include_in_menu_column2 );
-						const colThree = filter(e.children_data, e => e.include_in_menu_column3 );
-
-
 						return (
 							<li className={"nav_item"} key={i}>
 								{e.include_in_menu === 1
-									? <div className="panel-heading">
-											<a href={`/${e.url_path}`}>
-												<span>
-													{e.name}
-												</span>
-											</a>
-										</div>
+									? <React.Fragment>
+											<div className="panel-heading" id={`${e.url_path}Heading`}>
+												<a href={`/${e.url_path}`}>
+													<span>
+														{e.custom_category_name || e.name}
+													</span>
+												</a>
+											</div>
+											{e.children_data.length
+												? <Dropdown id={`${e.url_path}`} data={e.children_data} />
+												: null}
+										</React.Fragment>
 									: null}
-								<div className={"nav_dropdrown"}>
-									<div className={"nav_dropdrown_inner"}>
-										<div className={"nav_dropdrown_content"}>
-											<ul className="sub_category_list">
-												{/* 
-													if has  then more than 1 column
-													include_in_menu
-													include_in_menu_column2  
-													include_in_menu_column3
-												*/}
-
-												{e.children_data.map((e, i) => {
-													return (
-														<li key={i}>
-															{e.include_in_menu === 1
-																? <a href={`/${e.url_path}`}>
-																		<span>
-																			{e.custom_category_name
-																				? e.custom_category_name
-																				: e.name}
-																		</span>
-																	</a>
-																: null}
-														</li>
-													);
-												})}
-											</ul>
-										</div>
-									</div>
-								</div>
 							</li>
 						);
 					})}
@@ -109,4 +36,4 @@ class EmptyComp extends React.Component {
 	}
 }
 
-export default EmptyComp;
+export default Navigation;
